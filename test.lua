e597fa2da36e6a0489b5d2694551cfe968070c68
kmarius <froase@gmail.com>
2022-07-19 22:16:44 +0200

    update some of the tests

--- test.lua
+++ test.lua
@@ -2,118 +2,86 @@ local jsregexp = require("jsregexp")
 
 local tests = 0
 local fails = 0
-local success = 0
+local successes = 0
 
-local function test(str, regex, format, flags, want)
+-- TODO: print more info case on fail
+local function test(str, regex, flags, want)
 	tests = tests + 1
 	local r = jsregexp.compile(regex, flags)
-	if r then
+	if want and r then
 		local res = r(str)
-		for _, val in pairs(res) do
-			print("begin", val.begin)
-			print("_end", val._end)
-			print("length", val.length)
-			for k, v in pairs(val.groups) do
-				print(k, v)
+		if #res ~= #want then
+			print("match count mismatch: wanted", #want, "got ", #res)
+			fails = fails + 1
+			return
+		end
+		for i, val in pairs(res) do
+			local want = want[i]
+			if not want then
+				fails = fails + 1
+				return
+			end
+			local match = string.sub(str, val.begin_ind, val.end_ind-1)
+			if match ~= want[1] then
+				fails = fails + 1
+				print(match, want[1])
+				return
+			end
+			if #val.groups > 0 then
+				if not want.groups or #want.groups ~= #val.groups then
+					fails = fails + 1
+					return
+				end
+				for j, v in pairs(val.groups) do
+					if v ~= want.groups[j] then
+						fails = fails + 1
+					end
+				end
+			else
+				if want.groups and #want.groups > 0 then
+					fails = fails + 1
+					return
+				end
 			end
 		end
-		success = success + 1
+		successes = successes + 1
+	elseif not want and not r then
+		successes = successes + 1
+	else
+		fails = fails + 1
 	end
 end
 
-test(" ab yyy ac y ", "a([bc]) (y+)", "", "g", nil)
 
---
--- test("dummy", "(.*", "dummy", "", nil)
--- test("dummy", "[", "dummy", "", nil)
--- test("dummy", ".", "$", "", nil)
--- test("dummy", ".", "$ ", "", nil)
--- test("dummy", ".", "$a", "", nil)
--- test("dummy", ".", "${", "", nil)
--- test("dummy", ".", "${}", "", nil)
--- test("dummy", ".", "${ ", "", nil)
--- test("dummy", ".", "${ 1}", "", nil)
--- test("dummy", ".", "${1 }", "", nil)
--- test("dummy", ".", "${1", "", nil)
--- test("dummy", ".", "${-1", "", nil)
---
--- test("", "(\\s)|(\\w*)", "${1:+_}${2:/upcase}", "g", "")
--- test("shouldn't hang", "(\\w*)|shouldn't hang", "$1", "g", "shouldn't hang")
--- test("shouldn't hang", "(\\s*)|shouldn't hang", "$1", "g", "shouldn't hang")
---
--- test("ö", "ö", "", "", "")
--- test("äöü", "a", "", "", "äöü")
--- test("äöü", "(.*)", "${1:/upcase}", "", "ÄÖÜ")
--- test("ÄÖÜ", "(.*)", "${1:/downcase}", "", "äöü")
---
--- test("dummy", "c", "", "", "dummy")
--- test("dummy", "c", "", "g", "dummy")
--- test("dummy", "d", "", "", "ummy")
--- test("dummy", "d", "b", "", "bummy")
--- test("dummy", "m", "_", "", "du_my")
--- test("dummy", "m", "_", "g", "du__y")
---
--- test("dummy", "(.)", "${112837649182736541987325418976325417653120835641027}", "", "ummy")
--- test("dummy", "(.)", "${20}", "", "ummy")
--- test("dummy", "(.)", "$1", "", "dummy")
--- test("dummy", "(dummy)", "you ${1} yo", "", "you dummy yo")
--- test("dummy", "(dummy)", "you $1 yo", "", "you dummy yo")
--- test("dummy", "dummy", "you $0 yo", "", "you dummy yo")
--- test("dummy", "dummy", "you ${0} yo", "", "you dummy yo")
---
--- test("dummy", "(\\d*).*", "a${1:ha\\", "", nil)
--- test("dummy", "(\\d*).*", "a${1:ha\\}", "", nil)
--- test("dummy", "(\\d*).*", "${1:-ha}", "", "ha")
--- test("dummy", "(\\d*).*", "${1:ha}", "", "ha")
--- test("dummy", "(\\d*).*", "a${1:-ha}b", "", "ahab")
--- test("dummy", "(\\d*).*", "a${1:ha}b", "", "ahab")
--- test("dummy", "(\\d*).*", "a${1:ha\\}}b", "", "aha}b")
---
--- test("dummy", ".", "${1:+abc", "", nil)
--- test("dummy", ".", "${1:+", "", nil)
--- test("dummy", ".", "${1:+\\", "", nil)
--- test("dummy", ".", "${1:+\\}", "", nil)
--- test("dummy", "(\\d*).*", "${1:+}", "", "")
--- test(".dummy.", "(\\w+)", "a${1:+}b", "", ".ab.")
---
--- test("dummy", ".", "${1:?", "", nil)
--- test("dummy", ".", "${1:?:", "", nil)
--- test("dummy", ".", "${1:?:abc", "", nil)
--- test("dummy", ".", "${1:?abc", "", nil)
--- test("dummy", ".", "${1:?abc:", "", nil)
--- test("dummy", ".", "${1:?abc:def", "", nil)
--- test("dummy", ".", "${1:?abc}", "", nil)
--- test("dummy", "(.+)", "a${1:?yep:nope}b", "", "ayepb")
--- test("dummy", "(.+)", "${1:?yep:nope}", "", "yep")
--- test("dummy", "(\\d*).*", "${1:?yep:nope}", "", "nope")
--- test("dummy", "(.*)", "${1:?yep:}", "", "yep")
--- test("dummy", "(\\d*).*", "${1:?:nope}", "", "nope")
--- test("dummy", "(\\d+)", "${1:?yep:nope} ${1:-man} ${2:+ha}", "", "nope man ") -- no match, the whole string gets replaced with the format where each 'else' branch is used
--- test("dummy", "(\\d*)", "${1:?yep:nope }", "", "nope dummy") -- zero length string gets replaced at the beginning of "dummy"
---
--- test("dummy", ".", "${1:/yooo}", "", nil)
--- test("dummy", ".", "${1:/upcase }", "", nil)
--- test("dummy", ".", "${1:/upcase", "", nil)
--- test("dumMY", "(.*)", "a${1:/upcase}b", "", "aDUMMYb")
--- test("dumMY", "(.*)", "a${1:/upcase}b", "", "aDUMMYb")
--- test("dumMY", "(.*)", "a${1:/downcase}b", "", "adummyb")
--- test("aNOTher duMMy", "(.*)", "a${1:/capitalize}b", "", "aAnother Dummyb")
--- test("another dummy", "(\\w+)", "${1:/upcase}", "g", "ANOTHER DUMMY")
--- test("121212", "1", "a", "g", "a2a2a2")
---
--- test("d", ".", "\\$", "", "$")
--- test("d", ".", "\\${1:yee}", "", "${1:yee}")
--- test("d", ".", "\\$}", "", "$}")
--- test("d", ".", "\\\\", "", "\\")
---
--- test("strING STRING string", "(string)", "${1:+match}", "g", "strING STRING match")
--- test("strING STRING string", "(string)", "${1:+match}", "gi", "match match match")
+test("dummy", "(.*", "", nil)
+test("dummy", "[", "", nil)
+
+test("dummy", ".", "", {{"d"}})
+test("du", ".", "g", {{"d"}, {"u"}})
+
+test("dummy", "c", "", {})
+test("dummy", "c", "g", {})
+test("dummy", "d", "", {{"d"}})
+test("dummy", "m", "", {{"m"}})
+test("dummy", "m", "g", {{"m"}, {"m"}})
+
+-- no unicode yet
+-- test("äöü", ".", "g", {{"ä"}, {"ö"}, {"ü"}})
+-- test("äöü", "[äöü]", "g", {{"ä"}, {"ö"}, {"ü"}})
+-- test("äöü", ".", "", {{"ä"}})
+-- test("ÄÖÜ", ".", "", {{"Ä"}})
+
+test("dummy", "(dummy)", "", {{"dummy", groups = {"dummy"}}})
+
+test("The quick brown fox jumps over the lazy dog", "\\w+", "", {{"The"}})
+test("The quick brown fox jumps over the lazy dog", "\\w+", "g", {{"The"}, {"quick"}, {"brown"}, {"fox"}, {"jumps"}, {"over"}, {"the"}, {"lazy"}, {"dog"}})
+test("The quick brown fox jumps over the lazy dog", "[aeiou]{2,}", "g", {{"ui"}})
 
 local bold_green = "\27[1;32m"
 local bold_red = "\27[1;31m"
 local normal = "\27[0m"
 
 local color = fails == 0 and bold_green or bold_red
-print(string.format("%s%d tests run, %d successes, %d failed%s", color, tests, success, fails, normal))
+print(string.format("%s%d tests run, %d successes, %d failed%s", color, tests, successes, fails, normal))
 collectgarbage()
 os.exit(fails == 0 and 0 or 1)
