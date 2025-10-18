4d169a116a667c22c39fbb7146fd119cff240a19
kmarius <5224719+kmarius@users.noreply.github.com>
2025-10-18 10:45:42 +0200

    update libregexp

--- jsregexp.c
+++ jsregexp.c
@@ -35,8 +35,13 @@
 void *lre_realloc(void *opaque, void *ptr, size_t size) {
   return realloc(ptr, size);
 }
+
 BOOL lre_check_stack_overflow(void *opaque, size_t alloca_size) {
   return FALSE;
+}
+
+int lre_check_timeout(void *opaque) {
+  return 0;
 }
 
 struct regexp {
